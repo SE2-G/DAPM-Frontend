d5ff8a3ca16d58231684da33a8804946ae1631b6
Jia1019 <>
2024-10-22 09:37:35 +0200

    beauty the original appearance
    setting space of fornm and buttons

--- src/components/OverviewPage/Buttons/CreateRepositoryButton.tsx
+++ src/components/OverviewPage/Buttons/CreateRepositoryButton.tsx
@@ -60,12 +60,13 @@ const CreateRepositoryButton = ({ orgId }: CreateRepositoryButtonProps) => {
                             Create repository
                         </Typography>
                         <form onSubmit={handleSubmit}>
-                            <FormControl fullWidth margin="normal">
+                            <FormControl fullWidth margin="normal" style={{display: "flex", flexDirection: "column", gap: "15px"}}>
                                 <FormLabel>Repository name</FormLabel>
                                 <TextField name="Name" />
                             </FormControl>
-
-                            <Button type="submit" sx={{ backgroundColor: "gray", padding: "1px", color: "black" }}>Submit</Button>
+                            <div style={{textAlign: "center"}}>
+                                <Button type="submit" sx={{ backgroundColor: "grey", padding: "3px 7px", color: "white", display: "inline-block", verticalAlign: "middle" , marginTop: "20px", fontSize: "20px", fontWeight: "bolder"}}>Submit</Button>
+                            </div>
                         </form>
                     </Box>
                 </Box>
