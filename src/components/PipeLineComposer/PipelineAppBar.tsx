8b4885b57daa93a0f7ae0395e8abcfc2e8cbfd17
Toland <zyzytoland@gmail.com>
2024-10-29 01:04:18 +0100

    show status v1 complete

--- src/components/PipeLineComposer/PipelineAppBar.tsx
+++ src/components/PipeLineComposer/PipelineAppBar.tsx
@@ -17,6 +17,8 @@ export default function PipelineAppBar() {
   const dispatch = useDispatch();
 
   const [isEditing, setIsEditing] = useState(false);
+  const [statusMessage, setStatusMessage] = useState('');
+  const [statusType, setStatusType] = useState<'success' | 'error' | 'info'>('info');
 
   const handleStartEditing = () => {
     setIsEditing(true);
@@ -26,139 +28,181 @@ export default function PipelineAppBar() {
     setIsEditing(false);
   };
 
-  const organizations = useSelector(getOrganizations)
-  const repositories = useSelector(getRepositories)
+  const organizations = useSelector(getOrganizations);
+  const repositories = useSelector(getRepositories);
 
-  const pipelineName = useSelector(getActivePipeline)?.name
+  const pipelineName = useSelector(getActivePipeline)?.name;
 
   const setPipelineName = (name: string) => {
-    dispatch(updatePipelineName(name))
-  }
+    dispatch(updatePipelineName(name));
+  };
 
-  const flowData = useSelector(getActiveFlowData)
+  const flowData = useSelector(getActiveFlowData);
 
   const generateJson = async () => {
+    try {
+      setStatusMessage('Starting to deploy pipeline...');
+      setStatusType('info');
 
-    //console.log(flowData)
+      var edges = flowData!.edges.map(edge => {
+        return { sourceHandle: edge.sourceHandle, targetHandle: edge.targetHandle };
+      });
 
-    var edges = flowData!.edges.map(edge => {
-      return { sourceHandle: edge.sourceHandle, targetHandle: edge.targetHandle }
-    })
+      console.log('copied', edges);
 
-    console.log("copied", edges)
+      const dataSinks = flowData?.edges
+        .map((edge) => {
+          if (edge.data?.filename) {
+            const newTarget = getHandleId();
+            const edgeToModify = edges.find(
+              (e) => e.sourceHandle == edge.sourceHandle && e.targetHandle == edge.targetHandle
+            );
+            edgeToModify!.targetHandle = newTarget;
 
-    const dataSinks = flowData?.edges.map((edge) => {
-      if (edge.data?.filename) {
-        const newTarget = getHandleId()
-        const egeToModify = edges.find(e => e.sourceHandle == edge.sourceHandle && e.targetHandle == edge.targetHandle)
-        egeToModify!.targetHandle = newTarget
-
-        const originalDataSink = flowData!.nodes.find(node => node.id === edge.target) as Node<DataSinkNodeData>
-        return {
-          type: originalDataSink?.type,
-          data: {
-            ...originalDataSink?.data,
-            templateData: { sourceHandles: [], targetHandles: [{ id: newTarget }] },
-            instantiationData: {
-              resource: {
-                //...originalDataSink?.data?.instantiationData.repository, 
-                organizationId: originalDataSink?.data?.instantiationData.repository?.organizationId,
-                repositoryId: originalDataSink?.data?.instantiationData.repository?.id,
-                name: edge?.data?.filename
-              }
-            }
-          },
-          position: { x: 100, y: 100 },
-          id: getNodeId(),
-          width: 100,
-          height: 100,
-        }
-      }
-    }).filter(node => node !== undefined) as any
-
-    console.log(JSON.stringify(dataSinks))
-
-    const requestData = {
-      name: pipelineName,
-      pipeline: {
-        nodes: flowData?.nodes?.filter(node => node.type === 'dataSource').map(node => node as Node<DataSourceNodeData>).map(node => {
-          return {
-            type: node.type,
-            data: {
-              ...node.data,
-              instantiationData: {
-                resource: {
-                  //...node?.data?.instantiationData.resource,
-                  organizationId: node?.data?.instantiationData.resource?.organizationId,
-                  repositoryId: node?.data?.instantiationData.resource?.repositoryId,
-                  resourceId: node?.data?.instantiationData.resource?.id,
-                },
-              }
-            },
-            width: 100, height: 100, position: { x: 100, y: 100 }, id: node.id, label: "",
-          } as any
-        }).concat(
-          flowData?.nodes?.filter(node => node.type === 'operator').map(node => node as Node<OperatorNodeData>).map(node => {
+            const originalDataSink = flowData!.nodes.find(
+              (node) => node.id === edge.target
+            ) as Node<DataSinkNodeData>;
             return {
-              type: node.type, data: {
-                ...node.data,
+              type: originalDataSink?.type,
+              data: {
+                ...originalDataSink?.data,
+                templateData: { sourceHandles: [], targetHandles: [{ id: newTarget }] },
                 instantiationData: {
                   resource: {
-                    //...node?.data?.instantiationData.algorithm,
-                    organizationId: node?.data?.instantiationData.algorithm?.organizationId,
-                    repositoryId: node?.data?.instantiationData.algorithm?.repositoryId,
-                    resourceId: node?.data?.instantiationData.algorithm?.id,
-                  }
-                }
+                    organizationId:
+                      originalDataSink?.data?.instantiationData.repository?.organizationId,
+                    repositoryId: originalDataSink?.data?.instantiationData.repository?.id,
+                    name: edge?.data?.filename,
+                  },
+                },
               },
-              width: 100, height: 100, position: { x: 100, y: 100 }, id: node.id, label: "",
-            } as any
-          })
-        ).concat(dataSinks),
-        edges: edges.map(edge => {
-          return { sourceHandle: edge.sourceHandle, targetHandle: edge.targetHandle }
+              position: { x: 100, y: 100 },
+              id: getNodeId(),
+              width: 100,
+              height: 100,
+            };
+          }
         })
-      }
+        .filter((node) => node !== undefined) as any;
+
+      console.log(JSON.stringify(dataSinks));
+
+      const requestData = {
+        name: pipelineName,
+        pipeline: {
+          nodes: flowData?.nodes
+            ?.filter((node) => node.type === 'dataSource')
+            .map((node) => node as Node<DataSourceNodeData>)
+            .map((node) => {
+              return {
+                type: node.type,
+                data: {
+                  ...node.data,
+                  instantiationData: {
+                    resource: {
+                      organizationId: node?.data?.instantiationData.resource?.organizationId,
+                      repositoryId: node?.data?.instantiationData.resource?.repositoryId,
+                      resourceId: node?.data?.instantiationData.resource?.id,
+                    },
+                  },
+                },
+                width: 100,
+                height: 100,
+                position: { x: 100, y: 100 },
+                id: node.id,
+                label: '',
+              } as any;
+            })
+            .concat(
+              flowData?.nodes
+                ?.filter((node) => node.type === 'operator')
+                .map((node) => node as Node<OperatorNodeData>)
+                .map((node) => {
+                  return {
+                    type: node.type,
+                    data: {
+                      ...node.data,
+                      instantiationData: {
+                        resource: {
+                          organizationId:
+                            node?.data?.instantiationData.algorithm?.organizationId,
+                          repositoryId:
+                            node?.data?.instantiationData.algorithm?.repositoryId,
+                          resourceId: node?.data?.instantiationData.algorithm?.id,
+                        },
+                      },
+                    },
+                    width: 100,
+                    height: 100,
+                    position: { x: 100, y: 100 },
+                    id: node.id,
+                    label: '',
+                  } as any;
+                })
+            )
+            .concat(dataSinks),
+          edges: edges.map((edge) => {
+            return { sourceHandle: edge.sourceHandle, targetHandle: edge.targetHandle };
+          }),
+        },
+      };
+
+      console.log(JSON.stringify(requestData));
+
+      const selectedOrg = organizations[0];
+      const selectedRepo = repositories.filter((repo) => repo.organizationId === selectedOrg.id)[0];
+
+      console.log(`Selected organization ID: ${selectedOrg.id}`);
+      console.log(`Selected repository ID: ${selectedRepo.id}`);
+
+      setStatusMessage('Uploading pipeline...');
+      setStatusType('info');
+      const pipelineId = await putPipeline(selectedOrg.id, selectedRepo.id, requestData);
+
+      setStatusMessage('Creating execution instance...');
+      setStatusType('info');
+
+      const executionId = await putExecution(selectedOrg.id, selectedRepo.id, pipelineId);
+
+
+      setStatusMessage('Executing pipeline...');
+      setStatusType('info');
+
+      await putCommandStart(selectedOrg.id, selectedRepo.id, pipelineId, executionId);
+      setStatusMessage('Pipeline deployed successfully');
+      setStatusType('success');
+
+    } catch (error: any) {
+      setStatusMessage('Error: ' + error.message);
+      setStatusType('error');
     }
-
-    console.log(JSON.stringify(requestData))
-
-    const selectedOrg = organizations[0]
-    const selectedRepo = repositories.filter(repo => repo.organizationId === selectedOrg.id)[0]
-
-    const pipelineId = await putPipeline(selectedOrg.id, selectedRepo.id, requestData)
-    const executionId = await putExecution(selectedOrg.id, selectedRepo.id, pipelineId)
-    await putCommandStart(selectedOrg.id, selectedRepo.id, pipelineId, executionId)
-
-  }
+  };
 
   const generatePipelinedata = () => {
     const pipelineData = {
       nodes: flowData?.nodes || [],
-      edges: flowData?.edges || []
+      edges: flowData?.edges || [],
     };
-  
+
     const jsonString = JSON.stringify(pipelineData, null, 2);
-  
+
     const blob = new Blob([jsonString], { type: 'application/json' });
-  
+
     const url = URL.createObjectURL(blob);
-  
+
     const link = document.createElement('a');
     link.href = url;
     link.download = `${pipelineName || 'pipeline'}.json`;
     link.click();
-  
-    // Clean up the URL object
+
     URL.revokeObjectURL(url);
   };
-
 
   return (
     <AppBar position="fixed">
       <Toolbar sx={{ flexGrow: 1 }}>
         <Button onClick={() => navigate('/userpage')}>
-          <ArrowBackIosNewIcon sx={{ color: "white" }} />
+          <ArrowBackIosNewIcon sx={{ color: 'white' }} />
         </Button>
         <Box sx={{ width: '100%', textAlign: 'center' }}>
           {isEditing ? (
@@ -170,26 +214,54 @@ export default function PipelineAppBar() {
               inputProps={{ style: { textAlign: 'center', width: 'auto' } }}
             />
           ) : (
-            <Box onClick={handleStartEditing} sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', width: '100%' }}>
+            <Box
+              onClick={handleStartEditing}
+              sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'center',
+                width: '100%',
+              }}
+            >
               <Typography>{pipelineName}</Typography>
               <EditIcon sx={{ paddingLeft: '10px' }} />
             </Box>
           )}
         </Box>
 
-        <Button onClick={() => dispatch(toggleShowStatusEnable())}>
-          <Typography variant="body1" sx={{ color: "white" }}>Show status</Typography>
-        </Button>
-
         <Button onClick={() => generatePipelinedata()}>
-          <Typography variant="body1" sx={{ color: "green" }}>Export pipeline</Typography>
+          <Typography variant="body1" sx={{ color: 'green' }}>
+            Export pipeline
+          </Typography>
         </Button>
 
         <Button onClick={() => generateJson()}>
-          <Typography variant="body1" sx={{ color: "red" }}>Deploy pipeline</Typography>
+          <Typography variant="body1" sx={{ color: 'red' }}>
+            Deploy pipeline
+          </Typography>
         </Button>
       </Toolbar>
-    </AppBar>
-  )
-}
 
+      {statusMessage && (
+        <Box
+          sx={{
+            p: 2,
+            backgroundColor:
+              statusType === 'error'
+                ? 'red'
+                : statusType === 'success'
+                ? 'green'
+                : 'gray',
+            color: 'white',
+            position: 'fixed',
+            bottom: 0,
+            width: '100%',
+            textAlign: 'center',
+          }}
+        >
+          <Typography>{statusMessage}</Typography>
+        </Box>
+      )}
+    </AppBar>
+  );
+}
